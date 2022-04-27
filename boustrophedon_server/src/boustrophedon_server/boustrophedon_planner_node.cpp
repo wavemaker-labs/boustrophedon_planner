95fc79c1b2dc9f1cf84cd0ab731af945a67e5156
Prateek Sharma <prateek@clemson.edu>
2022-04-27 13:07:06 -0700

    sentry added in bplanner

--- boustrophedon_server/src/boustrophedon_server/boustrophedon_planner_node.cpp
+++ boustrophedon_server/src/boustrophedon_server/boustrophedon_planner_node.cpp
@@ -1,13 +1,17 @@
 #include <ros/ros.h>
 #include "boustrophedon_server/boustrophedon_planner_server.h"
+#include <sentry.h>
 
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "boustrophedon_planner_node");
+  sentry_options_t *options = sentry_options_new();
+  sentry_options_set_dsn(options, "https://1cb1bee385a84f38bd128de9c1d23baf@o1162461.ingest.sentry.io/6301844");
+  sentry_init(options);
 
   BoustrophedonPlannerServer server;
 
   ros::spin();
-
+  sentry_shutdown();
   return 0;
 }
